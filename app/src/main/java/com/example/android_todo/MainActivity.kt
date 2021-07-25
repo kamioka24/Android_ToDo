19cc7b4c37b3d3ba433a30c473fa3afa9ecb0f08
kamioka24 <keee2404@gmail.com>
2021-07-26 08:11:35 +0900

    [Update] ToolBar をセット

--- app/src/main/java/com/example/android_todo/MainActivity.kt
+++ app/src/main/java/com/example/android_todo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.android_todo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar.also {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                it.setTitleTextColor(getColor(R.color.white))
+                setSupportActionBar(it) // ツールバーをセット
+            }
+        }
     }
 }
