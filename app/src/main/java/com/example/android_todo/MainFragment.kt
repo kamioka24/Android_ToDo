398ce3302c422e43bd60eec0ce12e2105dea337a
kamioka24 <keee2404@gmail.com>
2021-07-26 22:01:10 +0900

    [Add] FAB Button

--- app/src/main/java/com/example/android_todo/MainFragment.kt
+++ app/src/main/java/com/example/android_todo/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
@@ -18,5 +20,13 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addTaskButton.setOnClickListener {
+            Toast.makeText(activity, "Add Tapped", Toast.LENGTH_SHORT).show()
+        }
     }
 }
